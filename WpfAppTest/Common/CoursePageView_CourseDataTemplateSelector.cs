4ad8f4242495cbed1df77cd74ceb2b7fd58656cc
yehongjun <h0149721@fihbj.cn>
2021-11-21 11:46:21 +0800

    2021.11.21-P72

---
+++ WpfAppTest/Common/CoursePageView_CourseDataTemplateSelector.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using WpfAppTest.Model;
+
+namespace WpfAppTest.Common
+{
+    public class CoursePageView_CourseDataTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate DefaultTemplate { get; set; }
+        public DataTemplate SkeletonTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if ((item as CoursePageView_CourseModel).IsShowSkeleton)
+            {
+                return SkeletonTemplate;
+            }
+            else
+            {
+                return DefaultTemplate;
+            }
+        }
+    }
+}
